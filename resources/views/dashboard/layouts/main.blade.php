a3c03d65da4a77b60072776ee1eac7c852d53f6b
zulfalsa <zulfa2805@gmail.com>
2025-07-07 10:29:15 +0700

    Update Sidebar Admin

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -18,14 +18,12 @@
 </head>
 
 <body>
-    @include('dashboard.layouts.header')
-
     <div class="container-fluid">
         <div class="row">
             @include('dashboard.layouts.sidebar')
         </div>
 
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <main class="col-md-9 ms-sm-auto col-lg-9 px-md-4">
             @yield('container')
         </main>
     </div>
