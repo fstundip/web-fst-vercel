d2bd827510754182e07d4243ee79ec8f00011711
zulfalsa <zulfa2805@gmail.com>
2025-07-09 12:02:36 +0700

    Update UI Mobile Informasi

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -13,12 +13,17 @@
             @foreach ($post->chunk(3) as $chunk)
                 <div class="row justify-content-center mb-4">
                     @foreach ($chunk as $postItem)
-                        <div class="col-12 col-sm-6 col-md-4 col-lg-4 mb-4">
+                        <div class="col-6 col-sm-4 col-md-4 col-lg-4 mb-4">
                             <a href="/informasi/{{ $postItem->category->slug }}/{{ $postItem->slug }}" class="card border-0 h-100 text-decoration-none rounded-4">
                                 <img src="{{ asset('storage/' . $postItem->image) }}" alt="{{ $postItem->title }}" class="img-fluid rounded-4" style="aspect-ratio: 16/9; object-fit: cover; object-position: center;">
                                 <div class="card-body text-dark">
                                     <span class="badge bg-succes mb-2">{{ $category->name }}</span>
-                                    <p class="card-title fw-semibold text-dark">{{ $postItem->title }}</p>
+                                    <p class="card-title fw-semibold d-sm-none">
+                                        {{ \Illuminate\Support\Str::limit($postItem->title, 22) }}
+                                    </p>
+                                    <p class="card-title fw-semibold d-none d-sm-block">
+                                        {{ $postItem->title }}
+                                    </p>
                                     <p class="text-muted mb-0">{{ $postItem->created_at->format('d F Y') }}</p>
                                 </div>
                                     
