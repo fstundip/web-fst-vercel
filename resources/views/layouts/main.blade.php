d48a582e00c45ba626349441e99ef990eb29c9a6
zulfalsa <zulfa2805@gmail.com>
2025-07-12 13:32:47 +0700

    Change Font Size Navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,7 +21,7 @@
     </div> -->
     <!-- NAVBAR  -->
     <nav class="navbar navbar-expand-lg sticky-top">
-        <div class="container" style="font-size: clamp(0.8rem, 1.5vw, 1rem);">
+        <div class="container">
             <a class="navbar-brand" href="/">
                 <img src="/img/fst-warna.png" alt="" width="50px">
             </a>
